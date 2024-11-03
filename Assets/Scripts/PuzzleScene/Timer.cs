cbe33676b6e11b902dd72e86cb29648575364d33
Gibson <Gibson570@hotmail.com>
2024-11-03 21:36:03 +0000

    Made timer need to be activated before it begins

--- Assets/Scripts/PuzzleScene/Timer.cs
+++ Assets/Scripts/PuzzleScene/Timer.cs
@@ -3,7 +3,18 @@ using UnityEngine;
 public class Timer: MonoBehaviour
 {
     private float TimePassed = 0;
+    private bool IsRunning = false;
     void Update(){
-      TimePassed = TimePassed += Time.deltaTime;
+      if (isRunning == true){
+        TimePassed = TimePassed += Time.deltaTime;
+      }
+    }
+
+    public void StartTimer(){
+      IsRunning = true;
+    }
+
+    public float GetCurrentTime(){
+      return TimePassed;
     }
 }
