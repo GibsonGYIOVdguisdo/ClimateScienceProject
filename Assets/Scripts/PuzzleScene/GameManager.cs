7ee9f7ac294663f1633c0cda2354d261c7a625aa
Gibson <Gibson570@hotmail.com>
2024-11-04 18:34:39 +0000

    Made beating the game load post game scene

--- Assets/Scripts/PuzzleScene/GameManager.cs
+++ Assets/Scripts/PuzzleScene/GameManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     Timer timer;
@@ -27,7 +29,7 @@ public class GameManager : MonoBehaviour
     }
     public void EndGame()
     {
-
+        SceneManager.LoadScene("PostPuzzle");
     }
 
     public static string GetImageLocation()
