7db7ea7d51eab345e4b0ae1f8f2781ad9aa6e40f
Gibson <Gibson570@hotmail.com>
2024-11-04 13:35:24 +0000

    Created function to find missing space

--- Assets/Scripts/PuzzleScene/TileManager.cs
+++ Assets/Scripts/PuzzleScene/TileManager.cs
@@ -4,13 +4,21 @@ using System.Linq;
 public class TileManager : MonoBehaviour
 {
     public GameObject[] Tiles;
-    public Vector2[] TileLocations = new Vector2[9];
+    public Vector2[] PossibleTileLocations = new Vector2[9];
+    public int[] TileLocations = new int[9];
+
     private int SelectedImageIndex = 0;
     private string SelectedImage;
+    private int EmptyTile;
     void Start()
     {
         LoadTileImages();
         RandomiseTiles();
+        Debug.Log(GetEmptySpace());
+    }
+
+    private void Update()
+    {
     }
 
     void LoadTileImages()
@@ -21,20 +29,35 @@ public class TileManager : MonoBehaviour
         SelectedImage = "Solarpower";
         for (int i = 0; i < Tiles.Length; i++)
         {
-            TileLocations[i] = Tiles[i].transform.position;
+            PossibleTileLocations[i] = Tiles[i].transform.position;
             Tiles[i].GetComponent<PuzzleTile>().SetSprite(sprites[i]);
         }
     }
 
     void RandomiseTiles()
     {
-        List<int> availableLocationIndexes = Enumerable.Range(0, TileLocations.Length).ToList();
+        List<int> availableLocationIndexes = Enumerable.Range(0, PossibleTileLocations.Length).ToList();
 
         for (int i = 0; i < Tiles.Length - 1; i++)
         {
             int selectedIndex = Random.Range(0, availableLocationIndexes.Count);
-            Tiles[i].GetComponent<PuzzleTile>().SetPosition(TileLocations[availableLocationIndexes[selectedIndex]]);
+            Tiles[i].GetComponent<PuzzleTile>().SetPosition(PossibleTileLocations[availableLocationIndexes[selectedIndex]]);
+            TileLocations[i] = availableLocationIndexes[selectedIndex];
             availableLocationIndexes.RemoveAt(selectedIndex);
         }
+        Destroy(Tiles[8].gameObject);
+        TileLocations[availableLocationIndexes[0]] = -1;
+    }
+
+    int GetEmptySpace()
+    {
+        for (int i = 0; i < TileLocations.Length; i++)
+        {
+            if (TileLocations[i] == -1)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
