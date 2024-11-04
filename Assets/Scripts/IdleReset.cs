6783dd9f2129147b3ac47702770da33edd3ec4f0
Gibson <Gibson570@hotmail.com>
2024-11-04 23:27:29 +0000

    Moved idle checks

---
+++ Assets/Scripts/IdleReset.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IdleReset : MonoBehaviour
+{
+    public float MaxIdleTime = 20;
+    float CurrentIdleTime = 0;
+	bool Running = false;
+
+    private void Update()
+	{
+		if (Running)
+		{
+			CurrentIdleTime += Time.deltaTime;
+			if (CurrentIdleTime > MaxIdleTime)
+			{
+				SceneManager.LoadScene("Puzzle");
+			}
+		}
+	}
+
+	public void StartTimer()
+	{
+		Running = true;
+	}
+	public void ResetTimer()
+	{
+		CurrentIdleTime = 0;
+    }
+
+}
