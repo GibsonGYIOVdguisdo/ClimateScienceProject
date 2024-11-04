09ffe8a82f5cf229f76f97c79cc3beb3b07bef2b
Gibson <Gibson570@hotmail.com>
2024-11-04 17:02:51 +0000

    Moved tile loading to GameManager

--- Assets/Scripts/PuzzleScene/GameManager.cs
+++ Assets/Scripts/PuzzleScene/GameManager.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
-
+using System;
+using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
     Timer timer;
+    TileManager TileManager;
+
+    static int SpriteIndex = 0;
+    public List<string> SpriteFileNames;
+    public List<string> SpriteTitles;
+    public List<string> SpriteDescriptions;
 
     private void Start()
     {
         timer = FindFirstObjectByType<Timer>();
+        TileManager = FindFirstObjectByType<TileManager>();
+        TileManager.LoadTileImages(SpriteFileNames[SpriteIndex]);
+        TileManager.RandomiseTiles();
     }
     public void StartGame()
     {
